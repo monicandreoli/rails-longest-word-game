7d53afec323ce683c1b0f3ada763cbdcf4acb15d
monica andreoli <monicandreoli@gmail.com>
2021-02-15 18:39:49 +0100

    style

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
-
-require 'json'
 require 'open-uri'
 
 class GamesController < ApplicationController
@@ -15,16 +13,18 @@ class GamesController < ApplicationController
 
     # The word can’t be built out of the original grid
     if !@word.each_char.all? { |c| @letters.include?(c) }
-      @message = "Sorry, but #{@word} can't be built out of #{@letters.join(', ')}"
+      @message = "Sorry, but #{@word} can't be built out of #{@letters.join(', ')} 😔"
+    elsif @word == ''
+      @message = "Sorry, but you didn't insert any word! 🤨"
     elsif url = "https://wagon-dictionary.herokuapp.com/#{@word}"
       # The word is valid according to the grid, but is not a valid English word
       file = JSON.parse(open(url).read)
       # word is false according to dictionary
       @message = if file['found'] == false
-                   "Sorry, but #{@word} does not seen to be a valid English word.."
+                   "Sorry, but #{@word} does not seen to be a valid English word.. 😥"
                  else
                    # The word is valid according to the grid and is an English word
-                   "Congratulation! #{@word} is a valid English word!"
+                   "Congratulations 🎉 #{@word} is a valid English word!"
                  end
     end
   end
